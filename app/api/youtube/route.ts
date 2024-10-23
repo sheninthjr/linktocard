8590c675ffd8ca13ea5cca8bc9c1f43e6ec3fe44
sheninthjr <sheninthjr23@gmail.com>
2024-10-23 11:33:37 +0530

    Route end modified

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -29,7 +29,21 @@ const downloadImage = async (url: string, fileName: string) => {
 const fetchYTProfile = async (userId: string) => {
     try {
         const profileUrl = `http://www.youtube.com/@${userId}`;
-        const { data } = await axios.get(profileUrl);
+        const { data } = await axios.get(profileUrl,{headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.9,ta;q=0.8',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Origin': 'http://localhost:3000',
+            'Referer': 'http://localhost:3000/home',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"'
+          },});
         const $ = cheerio.load(data);
         const imageSrc = $('link[rel="image_src"]').attr('href');
         return imageSrc;
@@ -41,7 +55,21 @@ const fetchYTProfile = async (userId: string) => {
 
 const fetchYouTubeData = async (url: string) => {
     try {
-        const { data } = await axios.get(url);
+        const { data } = await axios.get(url,{headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.9,ta;q=0.8',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Origin': 'http://localhost:3000',
+            'Referer': 'http://localhost:3000/home',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"'
+          },});
         const $ = cheerio.load(data);
         const title = $('meta[name="title"]').attr('content');
         const description = $('meta[name="description"]').attr('content');
@@ -49,6 +77,7 @@ const fetchYouTubeData = async (url: string) => {
         const userName = $('span[itemprop="author"] link[itemprop="name"]').attr('content');
         const id = $('span[itemprop="author"] link[itemprop="url"]').attr('href') || "";
         const userId = id?.split('http://www.youtube.com/@')[1];
+        console.log(title,description,imageSrc,userName)
         const imagePath = await downloadImage(imageSrc, `${userId}-thumbnail.jpg`);
         const profile = await fetchYTProfile(userId);
 
