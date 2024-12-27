b69f3388a2377fdeccc1e4ebe8d8e085ecfbfcbe
sheninthjr <sheninthjr23@gmail.com>
2024-12-27 23:09:01 +0530

    Cors Origin added

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,12 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
-
-  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set(
+    'Access-Control-Allow-Origin',
+    'https://linktopost.vercel.app',
+  );
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-
   return response;
 }
 
